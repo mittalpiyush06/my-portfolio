3a30ce45a1a46fd2e70c8f5457b472abc35faf43
Piyush Mittal <mittalpiyush0406@gmail.com>
2024-05-04 14:24:16 +0530

    feat: typescript setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { RightSection } from "./components/RightSection";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Left Section */}
+      <RightSection />
+      {/* Right Section */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RightSection.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const RightSection = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+}
